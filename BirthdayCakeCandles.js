c83939b06c01f6ccc724e435ab14e6eddf5561b9
Abhishek Nath <oovnath@gmail.com>
2021-12-26 18:56:43 +0530

    modify Birthday Cake

--- BirthdayCakeCandles.js
+++ BirthdayCakeCandles.js
@@ -1,3 +1,8 @@
+/*
+You are in charge of the cake for a child's birthday. You have decided the cake will have one candle for each year of their total age. 
+They will only be able to blow out the tallest of the candles. Count how many candles are tallest.
+*/ 
+
 let candles=[3,2,1,3];
 let total_candles=0;
 let bigCandles=Math.max.apply(Math, candles);
